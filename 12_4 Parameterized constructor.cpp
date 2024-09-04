574d033e05b0ff6cf393d9f24ab388c846b026cd
Sai Sankar Jena <saisankarjena4568@gmail.com>
2024-09-04 10:05:19 +0530

    Add files via upload

---
+++ 12_4 Parameterized constructor.cpp
@@ -0,0 +1,24 @@
+//Name: Srihari Nair
+//Prn: 23070123131
+//Class: EnTC B-2
+#include<iostream>
+using namespace std;
+class Num
+{
+    public:
+    Num(int c, int d)
+    {
+        if(c>d)
+        {
+            cout<<c<<" is greater";
+        }
+        else
+        {
+            cout<<d<<" is greater";
+        }
+    }
+};
+int main()
+{
+Num n1(4,3);
+}
